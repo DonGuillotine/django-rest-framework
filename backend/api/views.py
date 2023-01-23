59b7c0689adf2bc182b85a549d7ee9fb3220bac9
DonGuillotine <infect3dlab@gmail.com>
2023-01-23 14:58:05 +0100

    Converting to a DRF view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,14 @@
 import json
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from product.models import ProductModel
 
+@api_view(['GET'])
 def api_view(request):
     model_instance = ProductModel.objects.all().order_by('?').first()
     data = {}
     if model_instance:
         data = model_to_dict(model_instance, fields=['id', 'title'])
-    return JsonResponse(data)
+    return Response(data)
