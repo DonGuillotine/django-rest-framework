78e5192ee7dc97fe7e340b98423ba71a23813e7c
DonGuillotine <infect3dlab@gmail.com>
2023-01-23 14:45:46 +0100

    Converting the model instance to a Python dictionary II

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,11 @@
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from product.models import ProductModel
 
 def api_view(request):
     model_instance = ProductModel.objects.all().order_by('?').first()
     data = {}
     if model_instance:
-        data['id'] = model_instance.id
-        data['title'] = model_instance.title
-        data['content'] = model_instance.content
-        data['price'] = model_instance.price
+        data = model_to_dict(model_instance, fields=['id', 'title'])
     return JsonResponse(data)
